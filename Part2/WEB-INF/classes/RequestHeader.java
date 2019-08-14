fc3a43eae5e173b5ff13a092d6dafcc08755b210
Astha Sharma <asthasharma068@gmail.com>
2019-08-13 22:07:18 -0400

    Basic Servlet

---
+++ Part2/WEB-INF/classes/RequestHeader.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.*;
+
+public class RequestHeader extends HttpServlet {
+ 	public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+ 	response.setContentType("text/html");
+ 	PrintWriter out = response.getWriter();
+	 String title = "Showing Request Headers";
+ 	String docType ="<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 " +"Transitional//EN\">\n";
+ 	out.println(docType +"<HTML>\n" +"<HEAD><TITLE>" + title + "</TITLE></HEAD>\n" +"<BODY BGCOLOR=\"#FDF5E6\">\n" +"<H1 ALIGN=\"CENTER\">" + title + "</H1>\n" +"<B>Request Method: </B>" +request.getMethod() + "<BR>\n" +"<B>Request URI: </B>" +request.getRequestURI() + "<BR>\n" +"<B>Request Protocol: </B>" +request.getProtocol() + "<BR><BR>\n" + "<TABLE BORDER=2 ALIGN=\"CENTER\">\n" +"<TH>Header Name<TH>Header Value");
+ 	Enumeration headerNames = request.getHeaderNames();
+ 	while(headerNames.hasMoreElements()) {
+ 		String headerName = (String)headerNames.nextElement();
+ 		out.println("<TR><TD>" + headerName);
+		out.println(" <TD>" + request.getHeader(headerName));
+ 	}
+ 	out.println("</TABLE>\n</BODY></HTML>");
+ }
+
+ public void doPost(HttpServletRequest request,
+ HttpServletResponse response)
+ throws ServletException, IOException {
+ doGet(request, response);
+ }
+}
